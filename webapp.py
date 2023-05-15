ee95be1c12975e96f7dd4bb6763da967dbc55650
Jonathan Ji <99520267+JonathanMJi@users.noreply.github.com>
2023-05-14 22:23:24 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,16 +1,13 @@
 from markdown import markdown
 import streamlit as st
-from streamlit_option_menu import option_menu
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras import preprocessing
-import matplotlib.pyplot as plt
 from keras.utils.np_utils import normalize
 
 model = tf.keras.models.load_model('model.h5')
 
-def getResult(image):
+def detect(image):
     img = image
     img=img.resize((64,64))
     img = np.array(img)
@@ -18,37 +15,22 @@ def getResult(image):
     img = normalize(img, axis = 1)
     result =model.predict(img).ravel()
     if result > 0.8:
-        result = 0
-    else:
-        result = 1
-    return result
-
-def get_className(classNo):
-    Tumor_html="""
-        <div style="background-color:#F08080;padding:10px">
-        <h2 style="color:white;text-align:center;">Tumor detected</h2>
-        </div>
-        """
-    Normal_html="""
+        Normal_html="""
         <div style="background-color:#5ec391 ;padding:10px">
         <h3 style="color:white;text-align:center;">Tumor not detected</h3>
         </div>
         """
-    if classNo == 0:
         st.markdown(Normal_html, unsafe_allow_html=True)
-    elif classNo == 1:
+    else:
+        Tumor_html="""
+        <div style="background-color:#F08080;padding:10px">
+        <h2 style="color:white;text-align:center;">Tumor detected</h2>
+        </div>
+        """
         st.markdown(Tumor_html, unsafe_allow_html=True)
 
-
-
 def main():
-    st.title("AI Brain Tumor Detection Tool")
-    html_temp = """
-    <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Deep Learning Model</h2>
-    <h5 style="color:white;text-align:center;">This model utilizes CNN (Convolutional Neural Networks) for brain cancer detection.</h5>
-    </div>
-    """
+    st.title("SM2 IS Project: AI Brain Tumor Detection")
     st.markdown(html_temp, unsafe_allow_html=True)
     uploaded_file = st.file_uploader("", type=["png","jpg","jpeg"])
     s = f"""
@@ -65,27 +47,13 @@ def main():
 
     if class_btn:
         if uploaded_file is None:
-            st.write("Invalid command, please upload an image")
+            st.write("Invalid File Type")
         else:
-                predictions = getResult(image)
-                get_className(predictions)
-
-    help_html = """
-    <div style="background-color:#025246 ;padding:20px">
-    <h5 style="color:white;;text-align:center;">Info</h5>
-    <p style="color:white">This deep learning web application utilizes Convolutional Neural to identify the presence of brain tumors from patient MRIs.</p>
-    <p style="color:white">The dataset used to train this model is Ahmed Hamada's "Br35H :: Brain Tumor Detection 2020" on Kaggle.</p>
-    </div>
-    """
-    st.markdown(help_html, unsafe_allow_html=True)
-
+                pred = detect(image)
     def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
     local_css("style/style.css")
-
-                
 
 if __name__ == "__main__":
     main()
